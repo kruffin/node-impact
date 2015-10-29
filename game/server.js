0faed8f1d54bf2afe6a4ae0606907dc20a94a9e0
Kevin Ruffin <kruffin@gmail.com>
2015-10-28 21:33:50 -0400

    Adds in body-parser usage. Fixes the deprecated complaints from Express with req.param('') usage. Makes use of path.join() instead of +

--- game/server.js
+++ game/server.js
@@ -1,8 +1,11 @@
 var express = require('express'),
+	  bodyParser = require('body-parser'),
     path = require('path'),
-    impact = require('../lib'),
+    impact = require('jdm-node-impact'),
     port = 8080,
     app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Paths for Weltmesiter API
 app.get('/lib/weltmeister/api/glob.php', function(req, res){
@@ -18,8 +21,9 @@ app.get('/lib/weltmeister/api/save.php', function(req, res){
 });
 
 //Normal routing for ImpactJS
-app.use('/lib', express.static('impact_core/lib'));
-app.use('/media', express.static('impact_core/media'));
+console.log("directory: ", __dirname + '/impact_core/lib');
+app.use('/lib', express.static(__dirname + '/impact_core/lib'));
+app.use('/media', express.static(__dirname +'/impact_core/media'));
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/views/index.html'));
